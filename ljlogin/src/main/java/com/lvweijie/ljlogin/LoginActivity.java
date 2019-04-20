8072069b180fb1994f4137a30ae6ed2dd3e85dc0
weijielv <155404518@qq.com>
2019-04-20 22:28:15 +0800

    ignore

--- ljlogin/src/main/java/com/lvweijie/ljlogin/LoginActivity.java
+++ ljlogin/src/main/java/com/lvweijie/ljlogin/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.lvweijie.ljlogin;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
+
+import java.util.logging.Logger;
 
 @Route(path = "/login/loginActivity")
 public class LoginActivity extends AppCompatActivity {
@@ -14,6 +18,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Handler handler = new Handler(){
+
+        };
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                Log.e("login = ","delay 1s");
+            }
+        };
+        handler.postDelayed(r,1000);
         findViewById(R.id.login_tv_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
